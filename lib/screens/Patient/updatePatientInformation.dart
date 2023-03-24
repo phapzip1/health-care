353a1ddcf00c91a1dadc9259638c6db4b8598ca6
Đỗ Phạm Huy Khánh <20521442@gm.uit.edu.vn>
2023-03-24 11:51:25 +0700

    update user information page completed without theme

--- lib/screens/Patient/updatePatientInformation.dart
+++ lib/screens/Patient/updatePatientInformation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
 
 class UpdatePatientInfo extends StatefulWidget {
   const UpdatePatientInfo({super.key});
@@ -10,17 +11,17 @@ class UpdatePatientInfo extends StatefulWidget {
 class _UpdatePatientInfoState extends State<UpdatePatientInfo> {
   final circleAvatar = SizedBox(
     child: Container(
-      margin: EdgeInsets.only(bottom: 16),
-      child: CircleAvatar(
+      margin: const EdgeInsets.only(bottom: 16),
+      child: const CircleAvatar(
         radius: 56.0,
         backgroundColor: Colors.white,
         child: CircleAvatar(
-          child: Align(
+          child: const Align(
             alignment: Alignment.bottomRight,
-            child: CircleAvatar(
+            child: const CircleAvatar(
               backgroundColor: Colors.white,
               radius: 16.0,
-              child: Icon(
+              child: const Icon(
                 Icons.camera_alt,
                 size: 20.0,
                 color: Color(0xFF404040),
@@ -64,68 +65,132 @@ class _UpdatePatientInfoState extends State<UpdatePatientInfo> {
                 )),
           ],
         ),
-        body: Container(
+        body: SingleChildScrollView(
           padding: const EdgeInsets.all(16),
-
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+          child: Column(children: [
             circleAvatar,
-            const Text('Name', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            TextField(
-              style: const TextStyle(fontSize: 16),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Name',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  style: const TextStyle(fontSize: 16),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 20),
                   ),
                 ),
-            ),
-            const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
 
-            const Text('Phone number', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            TextField(
-              style: const TextStyle(fontSize: 16),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
+                const Text('Phone number',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  style: const TextStyle(fontSize: 16),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 20),
                   ),
                 ),
-            ),
-            const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
 
-            const Text('Gender', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            TextField(
-              style: const TextStyle(fontSize: 16),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
+                const Text('Gender',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 8,
+                ),
+
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black38, width: 1),
+                    borderRadius: BorderRadius.circular(
+                        50), 
+                  ),
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20, top: 4, bottom: 4),
+                      child: DropdownButton(
+                        // value: "Male",
+                        items: const [
+                          DropdownMenuItem(
+                            child: Text("Male"),
+                            value: "male",
+                          ),
+                          DropdownMenuItem(
+                            child: Text("Female"),
+                            value: "female",
+                          )
+                        ],
+                        onChanged: (value) {},
+
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+
+                        underline: Container(), 
+                        isExpanded: true, 
+                      )),
+                ),
+
+                const SizedBox(
+                  height: 20,
+                ),
+
+                const Text('Birthday',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  style: const TextStyle(fontSize: 16),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 20),
                   ),
                 ),
-            ),
-            const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
 
-            const Text('Birthday', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            TextField(
-              style: const TextStyle(fontSize: 16),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
+                const Text('Email',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  style: const TextStyle(fontSize: 16),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        vertical: 16, horizontal: 20),
                   ),
                 ),
+              ],
             ),
-            const SizedBox(height: 20,),
-
-            const Text('Email', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-            TextField(
-              style: const TextStyle(fontSize: 16),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-            ),
-            const SizedBox(height: 20,),
-
           ]),
         ));
   }
