f08bab1d7a2e02e917d03c25ea0ca0d537513817
Phắc Nguyễn <phapdeptrai2k2@gmail.com>
2023-06-30 23:23:48 +0700

    complete review model

--- lib/models/review_model.dart
+++ lib/models/review_model.dart
@@ -6,31 +6,30 @@ class ReviewModel {
   String doctorId;
   String patientId;
   String patientName;
-  int rating;
+  double rating;
   String feedback;
 
-  static final CollectionReference _ref =
-      FirebaseFirestore.instance.collection("review");
+  static final CollectionReference _ref = FirebaseFirestore.instance.collection("review");
 
-  ReviewModel(this.id, this.doctorId, this.patientId, this.patientName,
-      this.rating, this.feedback);
+  ReviewModel(this.id, this.doctorId, this.patientId, this.patientName, this.rating, this.feedback);
 
-  ReviewModel.create(this.doctorId, this.patientId, this.patientName,
-      this.rating, this.feedback);
+  ReviewModel.create(this.doctorId, this.patientId, this.patientName, this.rating, this.feedback);
 
   Future<void> save() async {
     try {
-      if (id == null) {
-        final docRef = await _ref.add({
-          "doctor_id": doctorId,
-          "patient_id": patientId,
-          "patient_name": patientName,
-          "rating": rating,
-          "feedback": feedback,
-        });
-        id = docRef.id;
+      if (id != null) {
+        _ref.doc(id).set(
+          {
+            "doctor_id": doctorId,
+            "patient_id": patientId,
+            "patient_name": patientName,
+            "rating": rating,
+            "feedback": feedback,
+          },
+          SetOptions(merge: true),
+        );
       } else {
-        await _ref.doc(id).set({
+        _ref.add({
           "doctor_id": doctorId,
           "patient_id": patientId,
           "patient_name": patientName,
@@ -41,6 +40,21 @@ class ReviewModel {
     } catch (e) {
       debugPrint(e.toString());
     }
+  }
+
+  static Future<ReviewModel?> getByDoctorIdAndPatientId(String doctorid, String patientid) async {
+    final snapshot = await _ref.where("doctor_id", isEqualTo: doctorid).where("patient_id", isEqualTo: patientid).get();
+    if (snapshot.size != 0) {
+      return ReviewModel(
+        snapshot.docs[0].id,
+        snapshot.docs[0].get("doctor_id"),
+        snapshot.docs[0].get("patient_id"),
+        snapshot.docs[0].get("patient_name"),
+        snapshot.docs[0].get("rating"),
+        snapshot.docs[0].get("feedback"),
+      );
+    }
+    return null;
   }
 
   Future<void> remove() async {
